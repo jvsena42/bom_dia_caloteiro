5b761cec5a23bb7dbd1621b659fceac8dcded867
jvsena42 <jvsena16@gmail.com>
2022-12-14 07:48:33 -0300

    feat: color palete

--- app/src/main/java/com/bulletapps/bomdiacaloteiro/ui/theme/Color.kt
+++ app/src/main/java/com/bulletapps/bomdiacaloteiro/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val primaryLight = Color(0xFFFFC107)
+val secondaryLight = Color(0xFFD39E00)
+val tertiaryLight = Color(0xFF0745FF)
+
+val primaryDark = Color(0xFF957206)
+val secondaryDark = Color(0xFF433303)
+val tertiaryDark = Color(0xFFE9B107)
